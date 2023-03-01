ab6a5f337bdc8c5f4ec474e88aec27fd2bd6735c
Viacheslav Plevako <cheryec@ukr.net>
2023-03-01 16:15:48 +0200

    Додав кольори

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,5 +1,9 @@
 import css from './Statistics.module.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
@@ -8,7 +12,12 @@ export const Statistics = ({ title, stats }) => {
       <ul className={css.stat_list}>
         {stats.map(el => (
           <>
-            <li className={css.item}>
+            <li
+              className={css.item}
+              style={{
+                backgroundColor: getRandomHexColor(),
+              }}
+            >
               <span className={css.label}>{el.label}</span>
               <span className={css.percentage}>{el.percentage}</span>
             </li>
